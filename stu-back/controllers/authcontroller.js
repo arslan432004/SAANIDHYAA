27cc8c3fdd250e9d24dcf4dd7d7803cb7e7bdb0f
DeepanshuNegi123 <deepanshu9760873328@gmail.com>
2025-11-17 11:30:38 +0530

    backend data here is feeded to database and data is send to frontend

--- stu-back/controllers/authcontroller.js
+++ stu-back/controllers/authcontroller.js
@@ -1,19 +1,18 @@
 
 
-const signupcontroller = async (req , res)=>{
+const signupcontroller = async (req, res) => {
+  try {
+    const body = req.body;
 
-    try {
+    return res.status(200).json({
+      message: "Data received successfully",
+      data: body
+    });
 
-res.send("signup");
-
-    }
-    catch(error){
-
-        console.log(error);
-
-    }
-
-}
-
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
 
 export default signupcontroller;
