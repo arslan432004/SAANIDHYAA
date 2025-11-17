d9924f8ab2ac13de4da34b2b6fd874cfd468f64e
DeepanshuNegi123 <deepanshu9760873328@gmail.com>
2025-11-17 11:28:52 +0530

    under routes i have authenticate to handle routes

--- stu-back/routes/authenticate.js
+++ stu-back/routes/authenticate.js
@@ -2,9 +2,8 @@ import express from "express";
 
 import signupcontroller from "../controllers/authcontroller.js"
 
-
 const router = express.Router();
 
-router.get("/signup",(signupcontroller));
+router.post("/signup",(signupcontroller));
 
 export default router;
