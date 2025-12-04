c443578e2f0110fe5664db80230a788dae3f4b32
DeepanshuNegi123 <deepanshu9760873328@gmail.com>
2025-12-04 23:23:11 +0530

    modified

--- stu-attendence/src/App.jsx
+++ stu-attendence/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
         {/* Protected Dashboard */}
         <Route
           path="/dashboard"
-          
+
           element={
         
               <Sidebar>
@@ -33,8 +33,7 @@ const App = () => {
           }
         />
 
-       
-
+    
       </Routes>
     </BrowserRouter>
   );
