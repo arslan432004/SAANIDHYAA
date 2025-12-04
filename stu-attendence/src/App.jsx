ae23e26200093b3f478f49101280d46f831ba1ba
DeepanshuNegi123 <deepanshu9760873328@gmail.com>
2025-12-04 23:14:23 +0530

    here i have ordered the routes

--- stu-attendence/src/App.jsx
+++ stu-attendence/src/App.jsx
@@ -2,38 +2,38 @@ import "./App.css";
 import Introslides from "./components/common/introslider";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import LoginPage from "./pages/Auth/LoginPage";
+// import SignupPage from "./pages/Auth/SignupPage";
 import Dashboard2 from "./pages/Dashboard/dashboard2";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/layout/Sidebar";
+// import ProtectedRoute from "./components/auth/ProtectedRoute";
+
 const App = () => {
   return (
     <BrowserRouter>
-  
       <Routes>
 
         {/* Intro Page */}
         <Route path="/" element={<Introslides />} />
 
-        {/* Login Page */}
+        {/* Login + Signup */}
         <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/signup" element={<SignupPage />} /> */}
 
-        {/* Dashboard Page */}
-     
-      <Route
-  path="/dashboard"
-  element={
-    <Sidebar>
-      <Dashboard />
-    </Sidebar>
-  }
-/>
+        {/* Protected Dashboard */}
+        <Route
+          path="/dashboard"
+          
+          element={
+        
+              <Sidebar>
+                <Dashboard />
+              </Sidebar>
+            
+          }
+        />
 
        
-
-        {/* Dashboard2 Page */}
-        <Route path="/dashboard2" element={<Dashboard2 />} />
-
-{/* <Route path="/sidebar" element={<Sidebar />} /> */}
 
       </Routes>
     </BrowserRouter>
