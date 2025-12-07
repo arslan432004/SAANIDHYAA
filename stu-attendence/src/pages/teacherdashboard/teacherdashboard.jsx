a0074be663988f84c46c77c7c5d12be543861442
DeepanshuNegi123 <deepanshu9760873328@gmail.com>
2025-12-08 02:19:51 +0530

    reformed toggling

---
+++ stu-attendence/src/pages/teacherdashboard/teacherdashboard.jsx
@@ -0,0 +1,34 @@
+import Sidebar from "../../components/teachercomponents/sidebar";
+import Navbar from "../../components/teachercomponents/navbar";
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
+
+const Teacherdashboard = () => {
+  const [isOpen, setIsOpen] = useState(false); // put in parent component so that al child componnents can get it 
+
+  return (
+    <div className="flex">
+
+      {/* SIDEBAR */}
+      {/* send the isOpen prop to the Sidebar component */}
+      <Sidebar isOpen={isOpen} toggle={() => setIsOpen(!isOpen)} /> 
+
+
+      {/* CONTENT AREA */}
+      <div
+        className={`flex flex-col w-full transition-all duration-300 
+        ${isOpen ? "ml-64" : "ml-0"}`}
+      >
+
+
+        {/* navabr same toggle function as side bar  */}
+        {/* menu button in navbar toggles the sidebar */}
+        <Navbar toggle={() => setIsOpen(!isOpen)} />
+        <Outlet />
+      </div>
+
+    </div>
+  );
+};
+
+export default Teacherdashboard;
