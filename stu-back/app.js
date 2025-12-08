2791d41443ec2d0f1a36dcfdfdf2595e4eafc232
DeepanshuNegi123 <deepanshu9760873328@gmail.com>
2025-12-08 15:52:23 +0530

    reformed app.js

--- stu-back/app.js
+++ stu-back/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import authenticate from "./routes/authenticate.js";
-
+import router from "./routes/authenticate.js";
 const app = express(); 
 app.use(express.json());
 app.use(cors({
@@ -13,12 +13,13 @@ app.use(cors({
 
 app.use(morgan("combined")); // Used morgan logger for the requests and responses.
 
-app.use(express.json()); 
 
 app.use("/api/auth", authenticate); 
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
 
 export default app;
