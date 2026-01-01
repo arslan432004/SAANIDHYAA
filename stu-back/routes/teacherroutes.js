9f00a5bc0c7fbe08cb79a85c59ee331c7ddbd513
arslan432004 <151603940+arslan432004@users.noreply.github.com>
2026-01-01 22:39:37 +0530

    Delete stu-back directory

--- stu-back/routes/teacherroutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {
-
-getProfile, updateProfile, getDashboard, getTodaysclasses, 
-getUpcomingclasses , getClassesbysemester, getClassesbydate
-}  from "../controllers/teachercontrollers.js";
-
-const router = express.Router();
-
